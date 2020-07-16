e589e0ac3cd7c5557cb4e9263e61cf22d19c1dfc
Courtland <courtland.griffin@sharp.com>
2020-07-15 17:08:42 -0700

    Project part 4 done!

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -13,12 +13,12 @@ const Yelp = {
             console.log(business);
             return {
               id: business.id,
-              imageSrc: business.imageSrc,
+              imageSrc: business.image_url,
               name: business.name,
               address: business.location.address1,
               city: business.location.city,
               state: business.location.state,
-              zipCode: business.location.zipCode,
+              zipCode: business.location.zip_code,
               category: business.categories[0].title,
               rating: business.rating,
               reviewCount: business.review_count,
